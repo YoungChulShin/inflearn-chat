ebe1490a2e27698bcf6b9cb64e183f33b9f80c7a
YoungChulShin <go1323@gmail.com>
2025-05-24 09:34:15 +0900

    chatserver: websocket 구현

---
+++ chatserver/src/main/java/study/spring/chatserver/chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package study.spring.chatserver.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  private final SimpleWebSocketHandler webSocketHandler;
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    // /connect url websocket 연결이 들어오면 handler 클래스가 처리한다.
+    registry
+        .addHandler(webSocketHandler, "/connect")
+        .setAllowedOrigins("http://localhost:3000")
+    ;
+  }
+}
